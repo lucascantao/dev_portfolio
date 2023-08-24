6a8e41ef28e833ed1a42f53881de6007bdd8f1f9
lucas cantao <cantao162@gmail.com>
2023-08-24 11:11:30 -0300

    menu navigation

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:estoque/pages/footer.dart';
 import 'package:estoque/pages/skill_knowledge.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:go_router/go_router.dart';
 
 import 'pages/done_projects.dart';
 import 'pages/resume.dart';
@@ -16,142 +17,143 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
   @override
   Widget build(BuildContext context) {
-    ScrollController scrollController = ScrollController();
+    final router = GoRouter(routes: [
+      GoRoute(
+          path: '/',
+          pageBuilder: (context, state) => const MaterialPage(child: Home()))
+    ]);
 
-    GlobalKey keySection1 = GlobalKey();
-    GlobalKey keySection2 = GlobalKey();
-    GlobalKey keySection3 = GlobalKey();
-    GlobalKey keySection4 = GlobalKey();
-
-    List<Widget> sections = [
-      Resume(
-        key: keySection1,
-      ),
-      DoneProjects(
-        key: keySection2,
-      ),
-      SkillKnowledge(
-        key: keySection3,
-      ),
-      About(
-        key: keySection4,
-      ),
-      Footer()
-    ];
-    return MaterialApp(
-      theme: ThemeData.dark(),
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        // appBar: AppBar(
-        //   title: const Text(
-        //     "Portfolio",
-        //     style: TextStyle(fontWeight: FontWeight.bold),
-        //   ),
-        //   toolbarHeight: MediaQuery.of(context).size.height * 0.1,
-        //   actions: [
-        //     Padding(
-        //       padding: const EdgeInsets.all(8.0),
-        //       child: TextButton(
-        //         onPressed: () async {
-        //           await Scrollable.ensureVisible(keySection1.currentContext!,
-        //               duration: const Duration(milliseconds: 600));
-        //         },
-        //         child: const Text("Resume"),
-        //       ),
-        //     ),
-        //     Padding(
-        //       padding: const EdgeInsets.all(8.0),
-        //       child: TextButton(
-        //         onPressed: () async {
-        //           await Scrollable.ensureVisible(keySection2.currentContext!,
-        //               duration: const Duration(milliseconds: 600));
-        //         },
-        //         child: const Text("Done Projects"),
-        //       ),
-        //     ),
-        //     Padding(
-        //       padding: const EdgeInsets.all(8.0),
-        //       child: TextButton(
-        //         onPressed: () async {
-        //           await Scrollable.ensureVisible(keySection3.currentContext!,
-        //               duration: const Duration(milliseconds: 600));
-        //         },
-        //         child: const Text("Skill"),
-        //       ),
-        //     ),
-        //     Padding(
-        //       padding: const EdgeInsets.all(8.0),
-        //       child: TextButton(
-        //         onPressed: () async {
-        //           await Scrollable.ensureVisible(keySection4.currentContext!,
-        //               duration: const Duration(milliseconds: 600));
-        //         },
-        //         child: const Text("About"),
-        //       ),
-        //     ),
-        //   ],
-        // ),
-        body: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          controller: scrollController,
-          slivers: <Widget>[
-            SliverAppBar(
-              floating: true,
-              snap: false,
-              pinned: false,
-              title: const Text("Portfolio"),
-              actions: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    onPressed: () async {
-                      await Scrollable.ensureVisible(
-                          keySection1.currentContext!,
-                          duration: const Duration(milliseconds: 600));
-                    },
-                    child: const Text("Resume"),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    onPressed: () async {
-                      await Scrollable.ensureVisible(
-                          keySection2.currentContext!,
-                          duration: const Duration(milliseconds: 600));
-                    },
-                    child: const Text("Done Projects"),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    onPressed: () async {
-                      await Scrollable.ensureVisible(
-                          keySection3.currentContext!,
-                          duration: const Duration(milliseconds: 600));
-                    },
-                    child: const Text("Skill"),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    onPressed: () async {
-                      await Scrollable.ensureVisible(
-                          keySection4.currentContext!,
-                          duration: const Duration(milliseconds: 600));
-                    },
-                    child: const Text("About"),
-                  ),
-                ),
-              ],
+      theme: ThemeData.dark(),
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
+    );
+  }
+}
+
+var scaffoldKey = GlobalKey<ScaffoldState>();
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<GlobalKey> keys = [
+      GlobalKey(),
+      GlobalKey(),
+      GlobalKey(),
+      GlobalKey(),
+    ];
+
+    return Scaffold(
+      key: scaffoldKey,
+      body: Stack(children: [
+        SingleChildScrollView(
+          child: Column(children: [
+            Resume(
+              key: keys[0],
+              nextKey: keys[1],
             ),
-            SliverList(
-                delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-              return Container(child: sections[index]);
-            }, childCount: sections.length))
+            DoneProjects(key: keys[1]),
+            SkillKnowledge(key: keys[2]),
+            About(key: keys[3]),
+            const Footer()
+          ]),
+        ),
+        SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [MenuNavigation(keys: keys)],
+            ))
+      ]),
+    );
+  }
+}
+
+class MenuNavigation extends StatefulWidget {
+  const MenuNavigation({super.key, required this.keys});
+
+  final List<GlobalKey<State<StatefulWidget>>> keys;
+
+  @override
+  State<StatefulWidget> createState() => _MenuNavigationState();
+}
+
+class _MenuNavigationState extends State<MenuNavigation> {
+  int selection = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12.0),
+        child: Column(
+          children: [
+            IconButton(
+                onPressed: () async {
+                  setState(() {
+                    selection = 1;
+                  });
+                  await Scrollable.ensureVisible(widget.keys[0].currentContext!,
+                      duration: const Duration(milliseconds: 500));
+                },
+                icon: Icon(
+                  selection == 1 ? Icons.square_sharp : Icons.square_outlined,
+                  color: Colors.amber,
+                )),
+            IconButton(
+              onPressed: () async {
+                setState(() {
+                  selection = 2;
+                });
+                await Scrollable.ensureVisible(widget.keys[1].currentContext!,
+                    duration: const Duration(milliseconds: 500));
+              },
+              icon: Icon(
+                  selection == 2 ? Icons.square_sharp : Icons.square_outlined,
+                  color: Colors.amber),
+            ),
+            IconButton(
+              onPressed: () async {
+                setState(() {
+                  selection = 3;
+                });
+                await Scrollable.ensureVisible(widget.keys[2].currentContext!,
+                    duration: const Duration(milliseconds: 500));
+              },
+              icon: Icon(
+                  selection == 3 ? Icons.square_sharp : Icons.square_outlined,
+                  color: Colors.amber),
+            ),
+            IconButton(
+              onPressed: () async {
+                setState(() {
+                  selection = 4;
+                });
+                await Scrollable.ensureVisible(widget.keys[3].currentContext!,
+                    duration: const Duration(milliseconds: 500));
+              },
+              icon: Icon(
+                  selection == 4 ? Icons.square_sharp : Icons.square_outlined,
+                  color: Colors.amber),
+            ),
+            IconButton(
+              onPressed: () async {
+                setState(() {
+                  selection = 5;
+                });
+                await Scrollable.ensureVisible(widget.keys[4].currentContext!,
+                    duration: const Duration(milliseconds: 500));
+              },
+              icon: Icon(
+                  selection == 5 ? Icons.square_sharp : Icons.square_outlined,
+                  color: Colors.amber),
+            )
           ],
         ),
       ),
